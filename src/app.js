afaa45864c3a84eab2f1b80816a1c3d2885c3fea
Kaquenduri <marchunque7880@gmail.com>
2026-05-18 21:33:15 -0500

    Fix 777

--- src/app.js
+++ src/app.js
@@ -18,8 +18,8 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
-const PORT = 8080 ;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Auth Service running on port ${PORT}`);
 });
