7cf8b3b26e6dcd1acac9dfb42ba9163227126e55
Kaquenduri <marchunque7880@gmail.com>
2026-05-18 21:10:04 -0500

    Fix Dockerfile security 1

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3000 ;
 
 app.listen(PORT, () => {
   console.log(`Auth Service running on port ${PORT}`);
